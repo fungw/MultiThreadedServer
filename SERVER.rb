aed1d40a38bc44918bef75d0cafbaf2706fa28bb
Wesley Fung <fungw@tcd.ie>
2015-10-10 08:16:46 +0100

    Working KILL_SERVICE messages. Closes both client and server when KILL_SERVICE message received. Working HELO message status, returns message and also client IP, client port to client.

--- SERVER.rb
+++ SERVER.rb
@@ -1,15 +1,38 @@
 class SERVER
 	require 'socket'
-
-	server = TCPServer.open('0.0.0.0', 8000)
+	address = '0.0.0.0'
+	port = ARGV[0] 
+	server = TCPServer.open('0.0.0.0', port)
+	puts "Server is running on #{address} on port #{port}." 
 
 	loop do                        
 		Thread.start(socket = server.accept) do |client|
 			request = socket.gets
-			STDERR.puts request
-			response = "Hello World!\n"
-			client.puts "Closing the connection. Bye!"
+
+			# GET /echo.php?message=hello HTTP/1.0
+			response = "Undefined, request error."
+			parseRequest = request.split('=')[1].strip
+			message = parseRequest.split(' HTTP')[0]
+			checkHELO = message.split('%20')[0].strip
+			if checkHELO == 'HELO' 
+				heloMessage = message.split('HELO%20')[1].strip
+			  sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr	
+				STDERR.puts 'HELO message recieved'
+				response = "#{message}\nIP:#{remote_ip}\nPort:#{remote_port}\nStudentID:\n"
+			end
+			if message == 'KILL_SERVICE'
+				STDERR.puts 'Kill status recieved, shutting down.'
+				response = 'Kill status recieved, server has been shut down.'
+				kill = true
+			end
+			STDERR.puts 'Request: ' + request
+			STDERR.puts 'Message: ' + message
+			STDERR.puts ' '
+			client.puts response 
 			client.close
+			if kill == true
+				exit
+			end
 		end
 	end
 end
---
+++ start.sh
@@ -0,0 +1,1 @@
+ruby SERVER.rb $1 
